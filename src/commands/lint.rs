29298ed2fc3c3e105bdbc96cea8ce75baaee05bc
azjezz <azjezz@protonmail.com>
2024-12-26 21:17:03 +0100

    chore: consistent flags
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- src/commands/lint.rs
+++ src/commands/lint.rs
@@ -44,17 +44,11 @@ or the default mode for a comprehensive analysis.
 )]
 pub struct LintCommand {
     /// Filter the output to only show issues that can be automatically fixed with `mago fix`.
-    #[arg(long, short, help = "filter the output to only show fixable issues", default_value_t = false)]
-    pub only_fixable: bool,
+    #[arg(long, short = 'f', help = "filter the output to only show fixable issues", default_value_t = false)]
+    pub fixable_only: bool,
 
     /// Perform only semantic analysis (parsing and semantic checks).
-    #[arg(
-        long,
-        short = 's',
-        default_value_t = false,
-        conflicts_with = "only-fixable",
-        help = "only perform parsing and semantic checks"
-    )]
+    #[arg(long, short = 's', help = "only perform parsing and semantic checks", default_value_t = false)]
     pub semantics_only: bool,
 
     /// Specify where the results should be reported.
@@ -92,7 +86,7 @@ pub async fn execute(command: LintCommand, configuration: Configuration) -> Resu
 
     let reporter = Reporter::new(interner, source_manager, command.reporting_target);
 
-    if command.only_fixable {
+    if command.fixable_only {
         reporter.report(issues.only_fixable(), command.reporting_format)?;
     } else {
         reporter.report(issues, command.reporting_format)?;
