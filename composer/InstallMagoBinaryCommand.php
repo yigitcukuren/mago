21895f682d5a6422d6262c9f33adafa3a375bb0e
Toon Verwerft <toonverwerft@gmail.com>
2025-01-24 02:12:46 +0100

    feat: introduce composer plugin (#35)

---
+++ composer/InstallMagoBinaryCommand.php
@@ -0,0 +1,269 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mago;
+
+use Composer\Command\BaseCommand;
+use Composer\InstalledVersions;
+use Composer\IO\ConsoleIO;
+use Composer\Util\Filesystem;
+use Composer\Util\Http\Response;
+use Composer\Util\HttpDownloader;
+use Composer\Util\ProcessExecutor;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class InstallMagoBinaryCommand extends BaseCommand
+{
+    protected function configure(): void
+    {
+        $this->setName('mago:install-binary');
+        $this->setDescription('Installs the mago binary for current platform.');
+        $this->addOption('force', 'f', InputOption::VALUE_NONE, 'Force re-installation of the mago binary.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $composer = $this->requireComposer();
+        $loop = $composer->getLoop();
+        $downloader = $loop->getHttpDownloader();
+        $io = $this->getIO();
+        $process_executor = $loop->getProcessExecutor();
+        assert(
+            $process_executor instanceof ProcessExecutor,
+            'Expecting a process executor, but none was found on the composer loop.',
+        );
+
+        try {
+            $release = $this->detectMagoReleaseId($downloader);
+            ['tag' => $release, 'downloads' => $downloads] = $this->buildAssetsMapForRelease($downloader, $release);
+            [
+                'platform' => $platform,
+                'executable' => $executable,
+                'storage_dir' => $storage_dir,
+            ] = $this->detectPlatformInfo($process_executor, $release);
+        } catch (\Throwable $e) {
+            $io->error($e->getMessage());
+            return self::FAILURE;
+        }
+
+        $download = $downloads[$storage_dir] ?? null;
+        if (!$download) {
+            $io->error("There is no mago {$release} download available for the current platform '{$platform}'.");
+            return self::FAILURE;
+        }
+
+        $filesystem = new Filesystem($process_executor);
+        $release_dir = __DIR__ . '/bin/' . $release;
+        $target_dir = $release_dir . '/' . $storage_dir;
+        $executable_path = $target_dir . '/' . $executable;
+        $executable_platform_file = __DIR__ . '/bin/.platform';
+        $executable_platform_content = $release . '/' . $storage_dir . '/' . $executable;
+
+        $filesystem->ensureDirectoryExists($release_dir);
+        if ($input->getOption('force')) {
+            $filesystem->emptyDirectory($target_dir);
+        }
+
+        if (file_exists($executable_path)) {
+            file_put_contents($executable_platform_file, $executable_platform_content);
+            $io->write("Mago {$release} binaries for platform '{$platform}' already exist.");
+            return self::SUCCESS;
+        }
+
+        $io->write("Downloading mago {$release} binary:");
+        $io->write('');
+
+        $io->write(" - {$download['file']}");
+
+        $downloaded_file = $release_dir . '/' . $download['file'];
+        $promise = $downloader->addCopy(
+            $download['url'],
+            $downloaded_file,
+        )->then(static function (Response $response) use (
+            $filesystem,
+            $release_dir,
+            $downloaded_file,
+            $executable_platform_file,
+            $executable_platform_content,
+        ): Response {
+            $phar = new \PharData($downloaded_file);
+            $phar->extractTo($release_dir);
+
+            $filesystem->remove($downloaded_file);
+
+            file_put_contents($executable_platform_file, $executable_platform_content);
+
+            return $response;
+        });
+
+        $io->write('');
+        $progress_bar = ($io instanceof ConsoleIO) ? $io->getProgressBar() : null;
+        $loop->wait([$promise], $progress_bar);
+        $io->write('');
+        $io->write('');
+        $io->write('Done!');
+
+        return self::SUCCESS;
+    }
+
+    private function detectMagoReleaseId(HttpDownloader $httpDownloader): string
+    {
+        $version = InstalledVersions::getVersion(MagoPlugin::PACKAGE_NAME);
+
+        $response = $httpDownloader->get($this->buildGithubApiUri('/releases?per_page=99999999999999999'));
+        $json = $response->decodeJson();
+
+        foreach ($json as $release) {
+            if ($release['tag_name'] === $version) {
+                return $release['id'];
+            }
+        }
+
+        return 'latest';
+    }
+
+    /**
+     * @return array{tag: string, downloads: array<string, {file: string, url: string}>}
+     */
+    private function buildAssetsMapForRelease(HttpDownloader $httpDownloader, string $releaseId): array
+    {
+        $response = $httpDownloader->get($this->buildGithubApiUri('/releases/' . $releaseId));
+        $json = $response->decodeJson();
+
+        return [
+            'tag' => $json['tag_name'],
+            'downloads' => array_reduce(
+                $json['assets'] ?? [],
+                /**
+                 * @param array<string, string> $downloadMap
+                 * @param array{browser_download_url: string, name: string} $asset
+                 * @return array<string, string>
+                 */
+                static function (array $downloadMap, array $asset): array {
+                    if (!str_ends_with($asset['name'], '.tar.gz') && !str_ends_with($asset['name'], '.zip')) {
+                        return $downloadMap;
+                    }
+
+                    $platform = preg_replace('{^(.*)(\.tar\.gz|\.zip)$}', '$1', $asset['name']);
+
+                    $downloadMap[$platform] = [
+                        'file' => $asset['name'],
+                        'url' => $asset['browser_download_url'],
+                    ];
+
+                    return $downloadMap;
+                },
+                [],
+            ),
+        ];
+    }
+
+    private function buildGithubApiUri(string $path): string
+    {
+        return 'https://api.github.com/repos/carthage-software/mago' . $path;
+    }
+
+    /**
+     * @return array{platform: string, executable: string, storage_dir: string}
+     */
+    private function detectPlatformInfo(ProcessExecutor $process_executor, string $version): array
+    {
+        $arch_name = strtolower(php_uname('m'));
+        $arch = match ($arch_name) {
+            'x86_64', 'amd64' => 'x86_64',
+            'arm64', 'aarch64' => 'aarch64',
+            'armv7l' => 'armv7',
+            'i386', 'i486', 'i586', 'i686' => 'i686',
+            'ppc' => 'powerpc',
+            'ppc64' => 'powerpc64',
+            'ppc64le' => 'powerpc64le',
+            's390x' => 's390x',
+            default
+                => throw new \RuntimeException(
+                "Unsupported machine architecture: {$arch_name}. Please open an issue on GitHub.",
+            ),
+        };
+
+        $os = strtolower(php_uname('s'));
+        $vendor = 'unknown';
+        $os_suffix = '';
+        $executable_extension = '';
+
+        switch ($os) {
+            case 'windows nt':
+                $os = 'windows';
+                $vendor = 'pc';
+                $os_suffix = 'msvc';
+                $executable_extension = '.exe';
+                break;
+            case 'darwin':
+                $vendor = 'apple';
+                break;
+            case 'linux':
+                if ($process_executor->execute('command -v ldd') === 0) {
+                    $process_executor->execute('ldd --version 2>&1', $ldd_version);
+                    if (strpos($ldd_version, 'musl') !== false) {
+                        switch ($arch) {
+                            case 'x86_64':
+                            case 'aarch64':
+                            case 'i686':
+                                $os_suffix = 'musl';
+                                break;
+                            case 'arm':
+                            case 'armv7':
+                                if (strpos(file_get_contents('/proc/cpuinfo'), 'hard') !== false) {
+                                    $os_suffix = 'musleabihf';
+                                } else {
+                                    $os_suffix = 'musleabi';
+                                }
+                                break;
+                            default:
+                                throw new \RuntimeException("Unsupported architecture for musl: {$arch_name}");
+                        }
+                    } else {
+                        switch ($arch) {
+                            case 'x86_64':
+                            case 'aarch64':
+                            case 'i686':
+                            case 'powerpc':
+                            case 'powerpc64':
+                            case 'powerpc64le':
+                            case 's390x':
+                                $os_suffix = 'gnu';
+                                break;
+                            case 'arm':
+                            case 'armv7':
+                                if (strpos(file_get_contents('/proc/cpuinfo'), 'hard') !== false) {
+                                    $os_suffix = 'gnueabihf';
+                                } else {
+                                    $os_suffix = 'gnueabi';
+                                }
+                                break;
+                            default:
+                                throw new \RuntimeException("Unsupported architecture for glibc: {$arch_name}");
+                        }
+                    }
+                } else {
+                    $os_suffix = 'musl';
+                }
+                break;
+            case 'freebsd':
+                break;
+            default:
+                throw new \RuntimeException("Unsupported operating system: {$os}. Please open an issue on GitHub.");
+        }
+
+        $target_triple = $os_suffix ? "{$arch}-{$vendor}-{$os}-{$os_suffix}" : "{$arch}-{$vendor}-{$os}";
+        $storage_dir = "mago-{$version}-{$target_triple}";
+        $executable = "mago{$executable_extension}";
+
+        return [
+            'platform' => $target_triple,
+            'storage_dir' => $storage_dir,
+            'executable' => $executable,
+        ];
+    }
+}
---
+++ composer/MagoPlugin.php
@@ -0,0 +1,101 @@
+<?php
+declare(strict_types=1);
+
+namespace Mago;
+
+use Composer\Composer;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
+use Composer\Installer\PackageEvents;
+use Composer\IO\IOInterface;
+use Composer\Plugin\Capability\CommandProvider;
+use Composer\Plugin\Capable;
+use Composer\Plugin\PluginInterface;
+use Composer\Util\ProcessExecutor;
+use Symfony\Component\Process\PhpExecutableFinder;
+
+final readonly class MagoPlugin implements PluginInterface, EventSubscriberInterface, Capable
+{
+    public const string PACKAGE_NAME = 'carthage-software/mago';
+
+    /**
+     * @inheritDoc
+     */
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    /**
+     * @return array<class-string, class-string>
+     */
+    public function getCapabilities(): array
+    {
+        return [
+            CommandProvider::class => MagoCommandProvider::class,
+        ];
+    }
+
+    /**
+     * Attach package installation events:.
+     *
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            PackageEvents::POST_PACKAGE_INSTALL => 'onPackageEvent',
+            PackageEvents::POST_PACKAGE_UPDATE => 'onPackageEvent',
+        ];
+    }
+
+    public function onPackageEvent(PackageEvent $event): void
+    {
+        if (!$this->isMagoPackageEvent($event)) {
+            return;
+        }
+
+        $composer = $event->getComposer();
+        $loop = $composer->getLoop();
+        $command_executor = $loop->getProcessExecutor();
+        assert(
+            $command_executor instanceof ProcessExecutor,
+            'Expecting a process executor, but none was found on the composer loop.',
+        );
+
+        $command_executor->executeTty(implode(' ', [
+            (new PhpExecutableFinder())->find(),
+            ...array_map(static fn(string $argument): string => ProcessExecutor::escape($argument), [
+                getenv('COMPOSER_BINARY') ?: 'composer',
+                'mago:install-binary',
+            ]),
+        ]));
+    }
+
+    private function isMagoPackageEvent(PackageEvent $event): bool
+    {
+        $operation = $event->getOperation();
+        $package = match (true) {
+            $operation instanceof UpdateOperation => $operation->getTargetPackage(),
+            $operation instanceof InstallOperation => $operation->getPackage(),
+            default => null,
+        };
+
+        return self::PACKAGE_NAME === $package?->getName();
+    }
+}
