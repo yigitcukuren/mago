bc9434fc5f2f021d76162c4d9f29a7e7799dd6ea
Saif Eddin Gmati <29315886+azjezz@users.noreply.github.com>
2025-06-28 18:05:14 +0100

    feat(config): add global configuration file (#222)
    Signed-off-by: azjezz <azjezz@protonmail.com>
    Co-authored-by: Quentin Jungo <quentin.jungo@gmail.com>

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,3 +1,4 @@
+use std::env::home_dir;
 use std::path::PathBuf;
 
 use config::Config;
@@ -64,9 +65,12 @@ impl Configuration {
     ///
     /// This function attempts to load the configuration from the following sources, in order of precedence:
     ///
-    /// 1. A TOML file specified by the `file` argument.
-    /// 2. A TOML file named `mago.toml` in the current directory.
-    /// 3. Environment variables with the prefix `MAGO_`.
+    /// 1. Environment variables with the prefix `MAGO_`.
+    /// 2. A TOML file specified by the `file` argument.
+    /// 3. A TOML file named `mago.toml` in the current directory.
+    /// 4. A TOML file named `mago.toml` in the `$HOME` directory.
+    ///
+    /// When the `file` argument is set, 3 and 4 are not used at all.
     ///
     /// The loaded configuration is then normalized and validated.
     ///
@@ -90,12 +94,20 @@ impl Configuration {
     ) -> Result<Configuration, Error> {
         let workspace_dir = workspace.clone().unwrap_or_else(|| CURRENT_DIR.to_path_buf());
 
-        let builder = Config::builder()
-            .add_source(match file {
-                Some(file) => File::from(file).required(true).format(FileFormat::Toml),
-                None => File::from(workspace_dir.join(CONFIGURATION_FILE)).required(false).format(FileFormat::Toml),
-            })
-            .add_source(Environment::with_prefix(ENVIRONMENT_PREFIX));
+        let mut builder = Config::builder();
+        if let Some(file) = file {
+            builder = builder.add_source(File::from(file).required(true).format(FileFormat::Toml));
+        } else {
+            if let Some(home_dir) = home_dir() {
+                builder = builder
+                    .add_source(File::from(home_dir.join(CONFIGURATION_FILE)).required(false).format(FileFormat::Toml));
+            }
+
+            builder = builder
+                .add_source(File::from(workspace_dir.join(CONFIGURATION_FILE)).required(false).format(FileFormat::Toml))
+        }
+
+        builder = builder.add_source(Environment::with_prefix(ENVIRONMENT_PREFIX));
 
         let mut configuration = Configuration::from_workspace(workspace_dir);
 
@@ -224,5 +236,114 @@ impl ConfigurationEntry for Configuration {
         self.linter.normalize()?;
 
         Ok(())
+    }
+}
+
+#[cfg(all(test, not(target_os = "windows")))]
+mod tests {
+    use std::fs;
+
+    use pretty_assertions::assert_eq;
+    use tempfile::env::temp_dir;
+
+    use super::*;
+
+    #[test]
+    fn test_take_defaults() {
+        let workspace_path = temp_dir().join("workspace-0");
+        std::fs::create_dir_all(&workspace_path).unwrap();
+
+        let config = temp_env::with_vars(
+            [
+                ("HOME", temp_dir().to_str()),
+                ("MAGO_THREADS", None),
+                ("MAGO_PHP_VERSION", None),
+                ("MAGO_ALLOW_UNSUPPORTED_PHP_VERSION", None),
+            ],
+            || Configuration::load(Some(workspace_path), None, None, None, false).unwrap(),
+        );
+
+        assert_eq!(config.threads, *LOGICAL_CPUS)
+    }
+
+    #[test]
+    fn test_env_config_override_all_others() {
+        let workspace_path = temp_dir().join("workspace-1");
+        let config_path = temp_dir().join("config-1");
+
+        std::fs::create_dir_all(&workspace_path).unwrap();
+        std::fs::create_dir_all(&config_path).unwrap();
+
+        let config_file_path = create_tmp_file("threads = 1", &config_path);
+        create_tmp_file("threads = 2", &workspace_path);
+
+        let config = temp_env::with_vars(
+            [
+                ("HOME", None),
+                ("MAGO_THREADS", Some("3")),
+                ("MAGO_PHP_VERSION", None),
+                ("MAGO_ALLOW_UNSUPPORTED_PHP_VERSION", None),
+            ],
+            || Configuration::load(Some(workspace_path), Some(config_file_path), None, None, false).unwrap(),
+        );
+
+        assert_eq!(config.threads, 3);
+    }
+
+    #[test]
+    fn test_config_cancel_workspace() {
+        let workspace_path = temp_dir().join("workspace-2");
+        let config_path = temp_dir().join("config-2");
+
+        std::fs::create_dir_all(&workspace_path).unwrap();
+        std::fs::create_dir_all(&config_path).unwrap();
+
+        create_tmp_file("threads = 2\nphp_version = \"7.4.0\"", &workspace_path);
+
+        let config_file_path = create_tmp_file("threads = 1", &config_path);
+        let config = temp_env::with_vars(
+            [
+                ("HOME", None::<&str>),
+                ("MAGO_THREADS", None),
+                ("MAGO_PHP_VERSION", None),
+                ("MAGO_ALLOW_UNSUPPORTED_PHP_VERSION", None),
+            ],
+            || Configuration::load(Some(workspace_path), Some(config_file_path), None, None, false).unwrap(),
+        );
+
+        assert_eq!(config.threads, 1);
+        assert_eq!(config.php_version.to_string(), DEFAULT_PHP_VERSION.to_string());
+    }
+
+    #[test]
+    fn test_merge_workspace_override_global() {
+        let home_path = temp_dir().join("home-3");
+        let workspace_path = temp_dir().join("workspace-3");
+
+        std::fs::create_dir_all(&home_path).unwrap();
+        std::fs::create_dir_all(&workspace_path).unwrap();
+
+        create_tmp_file("threads = 3\nphp_version = \"7.4.0\"", &home_path);
+        create_tmp_file("threads = 2", &workspace_path);
+
+        let config = temp_env::with_vars(
+            [
+                ("HOME", Some(home_path)),
+                ("MAGO_THREADS", None),
+                ("MAGO_PHP_VERSION", None),
+                ("MAGO_ALLOW_UNSUPPORTED_PHP_VERSION", None),
+            ],
+            || Configuration::load(Some(workspace_path), None, None, None, false).unwrap(),
+        );
+
+        assert_eq!(config.threads, 2);
+        assert_eq!(config.php_version.to_string(), "7.4.0".to_string());
+    }
+
+    fn create_tmp_file(config_content: &str, folder: &PathBuf) -> PathBuf {
+        fs::create_dir_all(folder).unwrap();
+        let config_path = folder.join(CONFIGURATION_FILE);
+        fs::write(&config_path, config_content).unwrap();
+        config_path
     }
 }
